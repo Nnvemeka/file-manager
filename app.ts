9d8f5423f41f89650df21afc94f51f2e5352501e
nnvedward <nnvedward@gmail.com>
2022-06-01 14:06:44 +0100

    Setup main module in express server

--- app.ts
+++ app.ts
@@ -1,73 +1,62 @@
 // eslint-disable-next-line
 require("dotenv").config()
+import { Directory, File, FileVersion } from "@prisma/client"
 import express from "express"
 import { graphqlHTTP } from 'express-graphql'
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import { PrismaClient } from '@prisma/client'
+import { createApplication, createModule, gql } from 'graphql-modules'
+import { directoryModule } from "./directory"
+import { fileVersionModule } from "./fileVersion"
+import { fileModule } from "./file"
 
-const prisma = new PrismaClient()
+const mainModule = createModule({
+  id: 'main-module',
+  dirname: __dirname,
+  typeDefs: [
+    gql`
+      interface FileNode {
+        id: ID!
+        name: String!
+        createdAt: String!
+        updatedAt: String!
+      }
 
-const typeDefs = `
-  type File {
-    id: String!
-    name: String!
-    directoryId: String!
-    createdAt: String!
-    updatedAt: String!
-    versions: [FileVersion]!
-  }
-
-  type FileVersion {
-    id: String!
-    name: String!
-    mimeType: String!
-    size: Int!
-    fileId: String!
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  type Directory {
-    id: String!
-    name: String!
-    parentId: String
-    createdAt: String!
-    updatedAt: String!
-    files: [File]!
-    directories: [Directory]!
-  }
-
-  type Query {
-    getAllFiles: [File]!
-    getAllFileVersions: [FileVersion]!
-    getAllDirectories: [Directory]!
-  }
-`
-
-const resolvers = {
-  Query: {
-    getAllFiles: () => {
-      return prisma.file.findMany()
+      type Query {
+        searchFiles(query: String!): [FileNode]
+      }
+    `
+  ],
+  resolvers: {
+    FileNode: {
+      __resolveType(obj: File | FileVersion | Directory) {
+        if (Object.prototype.hasOwnProperty.call(obj, "parentId")) {
+          return "Directory"
+        }
+        if (Object.prototype.hasOwnProperty.call(obj, "fileId")) {
+          return "FileVersion"
+        }
+        if (Object.prototype.hasOwnProperty.call(obj, "directoryId")) {
+          return "File"
+        }
+      }
     },
-    getAllFileVersions: () => {
-      return prisma.fileVersion.findMany()
-    },
-    getAllDirectories: () => {
-      return prisma.directory.findMany()
-    },
+    Query: {
+      searchFiles: () => {
+        return []
+      }
+    }
   }
-}
+})
 
-const schema = makeExecutableSchema({
-  resolvers,
-  typeDefs
+const api = createApplication({
+  modules: [mainModule, directoryModule, fileModule, fileVersionModule]
 })
 
 const app = express()
 const port = 4000
 
 app.use('/graphql', graphqlHTTP({
-  schema,
+  schema: api.schema,
+  customExecuteFn: api.createExecution(),
   graphiql: process.env.NODE_ENV === 'development'
 }))
 
