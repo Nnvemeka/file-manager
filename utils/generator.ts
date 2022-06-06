020820f80dff5de2a851edc0d068953eb48ff3e8
nnvedward <nnvedward@gmail.com>
2022-06-06 13:34:34 +0100

    Setup an ID generator

---
+++ utils/generator.ts
@@ -0,0 +1,5 @@
+import { customAlphabet } from 'nanoid/async'
+
+const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+const size = 11
+export const generateId = customAlphabet(alphabet, size);
